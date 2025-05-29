c201d3bdee07efa1daf23ff732802cdac6ddbf31
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2025-05-29 11:07:07 -0300

    JVV\feature_grafico_COFF_por_razao

--- Exploratoria Curtailment/le_curtailment.py
+++ Exploratoria Curtailment/le_curtailment.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def ler_csvs_input():
@@ -31,35 +32,173 @@ def ler_csvs_input():
 
     return dados_combinados
 
+# Gráfico de Geração Limitada para n pontos
+def plotar_geracao_limitada(dados, n):
+    """
+    Plota a geração limitada para os primeiros n pontos do DataFrame.
+
+    Args:
+        dados (pandas.DataFrame): DataFrame contendo os dados de geração limitada.
+        n (int): Número de pontos a serem plotados.
+    """
+    plt.figure(figsize=(10, 6))
+    plt.plot(dados['din_instante'].head(n), dados['val_geracaolimitada'].head(n))
+    plt.title('Geração Limitada em cada gerador(MW/30min)')
+    plt.xlabel('Data')
+    plt.ylabel('Geração Limitada')
+
+# Boxplot de Geração Limitada Agregada por hora
+def plotar_boxplot_geracao_limitada(dados):
+    """
+    Plota um boxplot da geração limitada agregada por hora.
+
+    Args:
+        dados (pandas.DataFrame): DataFrame contendo os dados de geração limitada.
+    """
+    dados['din_instante'] = pd.to_datetime(dados['din_instante'])
+    dados['hora'] = dados['din_instante'].dt.floor('H')                                     # Agrupa por hora
+    dados_agrupados = dados.groupby('hora', as_index=False)['val_geracaolimitada'].sum()    # Filtra os dados onde a geração limitada é maior que zero
+    
+    plt.figure(figsize=(10, 6))
+    plt.boxplot(dados_agrupados['val_geracaolimitada'], vert=True, patch_artist=True)
+    plt.title('Box Plot - Geração Limitada por Hora')
+    plt.xlabel('Hora')
+    plt.ylabel('Geração Limitada (MW)')
+
+# Avaliação por subsistema
+def plotar_geracao_limitada_por_subsistema(dados):
+    """
+    Plota a geração limitada agregada por subsistema.
+
+    Args:
+        dados (pandas.DataFrame): DataFrame contendo os dados de geração limitada.
+    """
+    # dados['din_instante'] = pd.to_datetime(dados['din_instante'])
+    # dados['hora'] = dados['din_instante'].dt.floor('H')             # Agrupa por hora
+    # Adiciona a semana consecutiva considerando o ano
+    dados['semana'] = (
+        (dados['din_instante'].dt.year - dados['din_instante'].dt.year.min()) * 52
+        + dados['din_instante'].dt.isocalendar().week
+    )
+    # Se for 1 de janeiro do primeiro ano, classifica como semana 0
+    primeiro_ano = dados['din_instante'].dt.year.min()
+    mask_primeiro_dia = (
+        (dados['din_instante'].dt.year == primeiro_ano) &
+        (dados['din_instante'].dt.month == 1) &
+        (dados['din_instante'].dt.day == 1)
+    )
+    dados.loc[mask_primeiro_dia, 'semana'] = 0
+    # dados_somados = dados.groupby(['hora', 'id_subsistema'], as_index=False)['val_geracaolimitada'].sum()
+    dados_somados = dados.groupby(['semana', 'id_subsistema'], as_index=False)['val_geracaolimitada'].sum()
+
+    for subsistema in dados_somados['id_subsistema'].unique():
+        plt.figure(figsize=(10, 6))
+        subset = dados_somados[dados_somados['id_subsistema'] == subsistema]
+        plt.bar(subset['semana'], subset['val_geracaolimitada'], label=subsistema)
+
+        plt.title("Geração Limitada no Subsistema " + subsistema)
+        plt.xlabel('Semana')
+        plt.ylabel('Geração Limitada (MW)')
+        plt.legend()
+
+# Avaliação por subsistema e razão de corte
+def razao_corte_por_subsistema(dados):
+    """
+    Plota a geração limitada agregada por subsistema.
+
+    Args:
+        dados (pandas.DataFrame): DataFrame contendo os dados de geração limitada.
+    """
+    # Adiciona a semana consecutiva considerando o ano
+    dados['semana'] = (
+        (dados['din_instante'].dt.year - dados['din_instante'].dt.year.min()) * 52
+        + dados['din_instante'].dt.isocalendar().week
+    )
+    # Se for 1 de janeiro do primeiro ano, classifica como semana 0
+    primeiro_ano = dados['din_instante'].dt.year.min()
+    mask_primeiro_dia = (
+        (dados['din_instante'].dt.year == primeiro_ano) &
+        (dados['din_instante'].dt.month == 1) &
+        (dados['din_instante'].dt.day == 1)
+    )
+    dados.loc[mask_primeiro_dia, 'semana'] = 0
+    dados_somados = dados.groupby(['semana', 'id_subsistema', 'cod_razaorestricao'], as_index=False)['val_geracaolimitada'].sum()
+
+    for subsistema in dados_somados['id_subsistema'].unique():
+        plt.figure(figsize=(10, 6))
+        df_sub = dados_somados[dados_somados['id_subsistema'] == subsistema]
+        semanas = sorted(df_sub['semana'].unique())
+        bottom = np.zeros(len(semanas))
+        restricoes = ['REL', 'ENE', 'CNF']
+        # for rest in df_sub['cod_razaorestricao'].unique():
+        for rest in restricoes:
+            subset = df_sub[df_sub['cod_razaorestricao'] == rest]
+            # Alinha os valores por semana
+            valores = [subset[subset['semana'] == s]['val_geracaolimitada'].sum()/2e3 for s in semanas]
+            plt.bar(semanas, valores, bottom=bottom, label=rest)
+            bottom += valores
+
+        plt.title("Geração Limitada no Subsistema " + subsistema)
+        plt.xlabel('Semana')
+        plt.ylabel('Geração Limitada (GWh)')
+        plt.legend()
+
+# Avaliação por razão de corte
+def razao_corte(dados):
+    """
+    Plota a geração limitada agregada por subsistema.
+
+    Args:
+        dados (pandas.DataFrame): DataFrame contendo os dados de geração limitada.
+    """
+    # Adiciona a semana consecutiva considerando o ano
+    dados['semana'] = (
+        (dados['din_instante'].dt.year - dados['din_instante'].dt.year.min()) * 52
+        + dados['din_instante'].dt.isocalendar().week
+    )
+    # Se for 1 de janeiro do primeiro ano, classifica como semana 0
+    primeiro_ano = dados['din_instante'].dt.year.min()
+    mask_primeiro_dia = (
+        (dados['din_instante'].dt.year == primeiro_ano) &
+        (dados['din_instante'].dt.month == 1) &
+        (dados['din_instante'].dt.day == 1)
+    )
+    dados.loc[mask_primeiro_dia, 'semana'] = 0
+    dados_somados = dados.groupby(['semana', 'cod_razaorestricao'], as_index=False)['val_geracaolimitada'].sum()
+
+    plt.figure(figsize=(10, 6))
+    semanas = sorted(dados_somados['semana'].unique())
+    bottom = np.zeros(len(semanas))
+    restricoes = ['REL', 'ENE', 'CNF']
+    # for rest in df_sub['cod_razaorestricao'].unique():
+    for rest in restricoes:
+        subset = dados_somados[dados_somados['cod_razaorestricao'] == rest]
+        # Alinha os valores por semana
+        valores = [subset[subset['semana'] == s]['val_geracaolimitada'].sum()/2e3 for s in semanas]
+        plt.bar(semanas, valores, bottom=bottom, label=rest)
+        bottom += valores
+
+    plt.title("Geração Limitada no SIN ")
+    plt.xlabel('Semana')
+    plt.ylabel('Geração Limitada (GWh)')
+    plt.legend()
+
+# Lê os dados dos arquivos CSV
 dados = ler_csvs_input()
 dados['din_instante']   = pd.to_datetime(dados['din_instante'])
 
 dados_24 = dados[dados['din_instante'].dt.year == 2024]     # Filtra os dados para o ano de 2024
 
-# Gráfico de Geração Limitada para n pontos
-n = 10000000
-plt.figure(figsize=(10, 6))
-plt.plot(dados['din_instante'].head(n),dados['val_geracaolimitada'].head(n))
-plt.title('Geração Limitada em cada gerador(MW/30min)')
-plt.xlabel('Data')
-plt.ylabel('Geração Limitada')
+n = 10000000    # Número de pontos a serem plotados
+# plotar_geracao_limitada(dados, n)
 
-# Boxplot de Geração Limitada Agregada por hora
-dados_agrupados = dados.groupby('din_instante', as_index=False)['val_geracaolimitada'].sum()            # Agrupa os dados por hora
-curtailment_sin = dados_agrupados[dados_agrupados['val_geracaolimitada'] > 0]['val_geracaolimitada']    # Filtra os dados onde a geração limitada é maior que zero
-plt.figure(figsize=(10, 6))
-plt.boxplot(curtailment_sin, vert=True, patch_artist=True)
-plt.title('Box Plot - Geração Limitada')
-plt.xlabel('Valores')
+# plotar_boxplot_geracao_limitada(dados)
 
-# Avaliação por subsistema
-dados_somados = dados.groupby(['din_instante', 'id_subsistema'], as_index=False)['val_geracaolimitada'].sum()
-plt.figure(figsize=(10, 6))
-dados_n = dados_somados['id_subsistema'] == "NE"
-plt.plot(dados_somados[dados_n]['din_instante'],dados_somados[dados_n]['val_geracaolimitada']/1e3)
-plt.title('Geração Limitada no Nordeste (GW/30min)')
-plt.xlabel('Data')
-plt.ylabel('Geração Limitada')
+# plotar_geracao_limitada_por_subsistema(dados)
+
+# razao_corte_por_subsistema(dados)
+
+razao_corte(dados)
 
 plt.show()
 
