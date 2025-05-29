a97ce25c6cba12bed0a7ff3966bbb60b3698a775
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2025-05-29 16:10:14 -0300

    JVV\archtecture_Codigo Modular
    Muda um pouco a arquitetura e torna o código modular. A biblioteca é o arquivo le_curtailment.py
    
    Além disso adicionei alguns dados de CMO semi-horário em outra pasta.

--- Exploratoria Curtailment/le_curtailment.py
+++ Exploratoria Curtailment/le_curtailment.py
@@ -204,33 +204,3 @@ def soma_corte_por_subsistema_e_razao(dados):
     plt.ylabel('Corte de Geração (TWh)')
     plt.ylim([0, 50])
     plt.legend()
-
-
-
-
-# Lê o DataFrame 'dados' do arquivo Parquet, se existir
-if os.path.exists("dados.parquet"):
-    dados = pd.read_parquet("dados.parquet")
-else:
-    dados = ler_csvs_input()
-    dados['din_instante'] = pd.to_datetime(dados['din_instante'])
-    dados.to_parquet("dados.parquet", index=False)
-dados_22e23 = dados[dados['din_instante'].dt.year < 2024]
-dados_24e25 = dados[dados['din_instante'].dt.year >= 2024]     # Filtra os dados para o ano de 2024
-
-n = 10000000    # Número de pontos a serem plotados
-# plotar_geracao_limitada(dados, n)
-
-# plotar_boxplot_geracao_limitada(dados)
-
-# plotar_geracao_limitada_por_subsistema(dados)
-
-razao_corte_por_subsistema(dados)
-
-# razao_corte(dados)
-
-# soma_corte_por_subsistema_e_razao(dados)
-
-plt.show()
-
-a=1
