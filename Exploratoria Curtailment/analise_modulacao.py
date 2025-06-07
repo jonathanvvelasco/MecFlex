5d20f1a4834fb8e202f993e723fe8466a3952b12
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2025-06-07 15:27:32 -0300

    Começa implementar modulação

--- Exploratoria Curtailment/analise_modulacao.py
+++ Exploratoria Curtailment/analise_modulacao.py
@@ -3,16 +3,16 @@ from le_curtailment import *
 from le_dadosnovos import *
 
 # Lê o DataFrame 'dados_coff' do arquivo Parquet, se existir
-if os.path.exists("dados_coff.parquet"):
-    dados_coff = pd.read_parquet("dados_coff.parquet")
-else:
-    dados_coff = ler_csvs_input()
-    dados_coff['din_instante'] = pd.to_datetime(dados_coff['din_instante'])
-    dados_coff.to_parquet("dados_coff.parquet", index=False)
+# if os.path.exists("dados_coff.parquet"):
+#     dados_coff = pd.read_parquet("dados_coff.parquet")
+# else:
+#     dados_coff = ler_csvs_input()
+#     dados_coff['din_instante'] = pd.to_datetime(dados_coff['din_instante'])
+#     dados_coff.to_parquet("dados_coff.parquet", index=False)
 # dados_coff_22e23 = dados_coff[dados_coff['din_instante'].dt.year < 2024]
 # dados_coff_24e25 = dados_coff[dados_coff['din_instante'].dt.year >= 2024]     # Filtra os dados_coff para o ano de 2024
 
-dados = le_parquet()
+dados, dados_coff = le_dados()
 
 n = 10000000    # Número de pontos a serem plotados
 # plotar_geracao_limitada(dados_coff, n)
@@ -27,7 +27,7 @@ n = 10000000    # Número de pontos a serem plotados
 
 # soma_corte_por_subsistema_e_razao(dados_coff)
 
-compara_demanda(dados, dados_coff)
+# compara_demanda(dados, dados_coff)
 
 plt.show()
 
--- Exploratoria Curtailment/le_dadosnovos.py
+++ Exploratoria Curtailment/le_dadosnovos.py
@@ -1,16 +1,27 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from le_curtailment import ler_csvs_input
 
-def le_parquet():
+def le_dados():
+    if os.path.exists("dados_coff.parquet"):
+        dados_coff = pd.read_parquet("dados_coff.parquet")
+    else:
+        dados_coff = ler_csvs_input()
+        dados_coff['din_instante'] = pd.to_datetime(dados_coff['din_instante'])
+        dados_coff.to_parquet("dados_coff.parquet", index=False)
+
     # Lê todos os arquivos parquet na pasta atual
-    pasta_atual = os.path.dirname(os.path.abspath(__file__))
-    pasta_dnovos = os.path.join(pasta_atual, "Dados Novos")
-    os.chdir(pasta_dnovos)
-    arquivos_parquet = [f for f in os.listdir() if f.endswith('.parquet')]
-    lista_dfs = [pd.read_parquet(f) for f in arquivos_parquet]
-    dados = pd.concat(lista_dfs, ignore_index=True)
-    return dados
+    if os.path.exists("dados.parquet"):
+        dados = pd.read_parquet("dados.parquet")
+    else:
+        pasta_atual = os.path.dirname(os.path.abspath(__file__))
+        pasta_dnovos = os.path.join(pasta_atual, "Dados Novos")
+        os.chdir(pasta_dnovos)
+        arquivos_parquet = [f for f in os.listdir() if f.endswith('.parquet')]
+        lista_dfs = [pd.read_parquet(f) for f in arquivos_parquet]
+        dados = pd.concat(lista_dfs, ignore_index=True)
+    return dados, dados_coff
 
 def compara_demanda(dados, dados_coff):
     '''Compara a demanda de energia entre dois períodos diferentes.'''
@@ -31,3 +42,10 @@ def trata_dados(dados,dados_coff):
     dados2 = pd.merge(carga_ne, coff_ne_ene, on='din_instante', how='left')
     dados['val_geracaolimitada'] = dados['val_geracaolimitada'].fillna(0)
     return dados
+
+def modulacao(dados):
+    '''Modula a geração de energia no subsistema Nordeste.'''
+    subsistema = 'NE'
+    dados_subsistema = dados[dados['id_subsistema'] == subsistema]
+    carga_subsistema = dados_subsistema[dados_subsistema['val_cargaenergiahomwmed']>=0]
+    return carga_subsistema
